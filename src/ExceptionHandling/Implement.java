b6eb4beee95ab2a8665221c2ff554b129c6cdb54
Dheeraj Singh <dhrax21bc@gmail.com>
2023-01-02 20:05:42 +0530

    exception code

---
+++ src/ExceptionHandling/Implement.java
@@ -0,0 +1,28 @@
+package ExceptionHandling;
+
+public class Implement {
+
+    public static void main(String[] args) {
+//        int a=8;
+//        int b=0;
+//        int[] arr=new int[2];
+        String s=null;
+
+        try {
+            System.out.println(s.length());
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("1");
+        }
+        catch (Exception e){
+            System.out.println("2");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        finally {
+            System.out.println("definite");
+        }
+
+    }
+}
