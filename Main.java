c9ef5ddd9bfc56cc17af835ccb886e2e04b82653
Dheeraj Singh <dhrax21bc@gmail.com>
2023-12-10 16:20:02 +0530

    after commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        Scanner sc=new Scanner(System.in);
+
+          int[] arr={8,1,7,3,-5};
+          Sorting.insertSort(arr);
+
+
+    }
+
+
+
+}
